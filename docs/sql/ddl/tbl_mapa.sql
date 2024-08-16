7f33f87f4a55a8490a22ff467bead363686941b3
ShaineOliveira <87617726+ShaineOliveira@users.noreply.github.com>
2024-08-16 00:21:51 -0300

    Update tbl_mapa.sql

--- docs/sql/ddl/tbl_mapa.sql
+++ docs/sql/ddl/tbl_mapa.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS public.mapa
     nome VARCHAR(255) COLLATE pg_catalog."default" NOT NULL,
     quantidade_andares integer NOT NULL,
     CONSTRAINT mapa_pkey PRIMARY KEY (nome)
-)
+);
